1a9c5f7be05e0edd16a29c7d0744e02c4f18d007
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-12-15 12:43:55 -0800

    pw hashing with hook

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
 
 // create our User model
 class User extends Model {}
@@ -7,19 +9,19 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
     {
-      // define an id column
+      
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
       },
-      // define a username column
+  
       username: {
         type: DataTypes.STRING,
         allowNull: false
       },
-      // define an email column
+      
       email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -28,17 +30,30 @@ User.init(
           isEmail: true
         }
       },
-      // define a password column
+      
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
-          // this means the password must be at least four characters long
           len: [4]
         }
       }
     },
     {
+      hooks: {
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+
+      },
+
+    
       sequelize,
       timestamps: false,
       freezeTableName: true,
